773d175746d549039c1d806815af4e28b219df0b
khushibedar <khushi.bedar@tmdc.io>
2026-07-29 07:45:23 +0000

    Update SQL for ops_to_activities

--- cadp/model/sqls/ops_to_activities.sql
+++ cadp/model/sqls/ops_to_activities.sql
@@ -1,1 +1,13 @@
-SELECT * FROM icebase.builderuploads.ops_to_activities
+SELECT 
+    id,
+    agency,
+    account_name,
+    opportunity_name,
+    activity_name,
+    activity,
+    created_by,
+    created_date,
+    type,
+    practice_area,
+    concat(id,coalesce(activity, 'no-info'), activity_name) as unique_id
+FROM icebase.builderuploads.ops_to_activities
